63844d5c62330a52a34f935ecf6c1102a12b3aac
dlcisme <34501019+dlcisme@users.noreply.github.com>
2019-01-04 12:59:09 -0600

    Create post_install.sh

---
+++ post_install.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+# set the data location
+DATA_LOCATION="/app-data/tautulli"
+
+# create the data location
+mkdir $DATA_LOCATION
+
+# make "tautulli" the owner of the data location
+chown -R tautulli:tautulli $DATA_LOCATION
+
+# give write permission for plugin update
+chmod 755 $DATA_LOCATION
+
+# give execute permssion to the Daemon script
+chmod u+x /usr/local/etc/rc.d/tautulli
+
+# enable jackett to start at boot
+sysrc "tautulli_enable=YES"
+
+# start the jackett service
+service tautulli start
